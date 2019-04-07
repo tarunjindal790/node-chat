fdfe11bd496a8f92c86996ac989a3619df347126
tarunjindal790 <tarunjindal790@gmail.com>
2019-04-07 16:54:04 +0530

    Console chat

--- server/app.js
+++ server/app.js
@@ -1,11 +1,32 @@
 const path=require('path');
+const http=require('http');
 const express=require('express');
+const socketIO=require('socket.io');
 const publicPath=path.join(__dirname,'../public');
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 3001;
 var app=express();
-
+var server=http.createServer(app);
+var io=socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+	console.log("New User Connected");
+	socket.on('disconnect',()=>{
+		console.log("User disconnected");
+	})
+	
+	socket.on('createMessage',(message)=>{
+	console.log("Message received",message);
+	io.emit('newMessage',{
+		from:message.from,
+		text:message.text,
+		createdAt:new Date().getTime()
+	})
+})
+})
+
+
+
+server.listen(port,()=>{
 	console.log('Server is running...');
 })
