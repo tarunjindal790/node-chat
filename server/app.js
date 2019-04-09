dce0a47fa418c05761f8773d038f9a523e2e54e0
tarunjindal790 <tarunjindal790@gmail.com>
2019-04-10 01:57:41 +0530

    Add geolocation support

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const path=require('path');
 const http=require('http');
 const express=require('express');
 const socketIO=require('socket.io');
-const{generateMessage}=require('./utils/message');
+const{generateMessage,generateLocationMessage}=require('./utils/message');
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT || 3001;
 var app=express();
@@ -21,6 +21,10 @@ io.on('connection',(socket)=>{
 	console.log("Message received",message);
 	io.emit('newMessage',generateMessage(message.from,message.text));
 })
+	socket.on('createLocationMessage',(coords)=>{
+		io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude))
+	})
+
 	socket.on('disconnect',()=>{
 		console.log("User disconnected");
 	})
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect=require('expect');
 
-var{generateMessage}=require('./message');
+var{generateMessage,generateLocationMessage}=require('./message');
 
 describe('generateMessage',()=>{
 	it('should generate correct message object',()=>{
@@ -12,4 +12,16 @@ describe('generateMessage',()=>{
 		expect(message).toMatchObject({from,text})
 	})
 
+})
+
+describe('generateLocationMessage',()=>{
+	it('should generate correct location message object',()=>{
+		var from='Jen';
+		var latitude=1;
+		var longitude=2;
+		var url='https://www.google.com/maps?q=1,2';
+		var message=generateLocationMessage(from,latitude,longitude);
+		expect(typeof message.createdAt).toBe('number');
+		expect(message).toMatchObject({from,url});
+	})
 })
